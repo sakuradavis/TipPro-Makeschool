7ae4decf7c4149da105b9d4f4ae1270b0c2eb3f0
sakura.davis <you@yourdomain.com>
2016-06-30 13:57:10 -0400

    TipPro
    Swift.

--- TipPro/ViewController.swift
+++ TipPro/ViewController.swift
@@ -10,7 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet var billAmountField: UITextField!
+    
+    @IBOutlet var tipSelector: UISegmentedControl!
+    
+    @IBOutlet var tipAmountField: UITextField!
+    
+    @IBOutlet var totalAmountField: UITextField!
+    
+    @IBAction func calculateTip(sender: AnyObject) {
+        guard let billAmount = Double(billAmountField.text!) else {
+            //show error
+            billAmountField.text = ""
+            tipAmountField.text = ""
+            totalAmountField.text = ""
+            return
+        }
+        
+        var tipPercentage = 0.0
+        
+        switch tipSelector.selectedSegmentIndex {
+        case 0:
+            tipPercentage = 0.15
+        case 1:
+            tipPercentage = 0.18
+        case 2:
+            tipPercentage = 0.20
+        default:
+            break
+        }
+        
+        let roundedBillAmount = round(100*billAmount)/100
+        let tipAmount = roundedBillAmount * tipPercentage
+        let roundedTipAmount = round(100*tipAmount)/100
+        let totalAmount = roundedBillAmount + roundedTipAmount
+        
+        if (!billAmountField.editing) {
+            billAmountField.text = String(format: "%.2f", roundedBillAmount)
+        }
+        tipAmountField.text = String(format: "%.2f", roundedTipAmount)
+        totalAmountField.text = String(format: "%.2f", totalAmount)
+    }
+    
+     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -18,6 +60,10 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
     }
 
 
